8b7c046e36ab855e3b36ee51339dac081b14f1f2
Nik Petersen <npetersen@viciware.com>
2015-12-01 10:00:40 -0800

    nginx proxy app image

--- nginx/proxy-app/run.sh
+++ nginx/proxy-app/run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
-echo $URI_TO_SCRIPT
 if [ -n $URI_TO_SCRIPT ]
 then
   sed -i "/try_files /c try_files \$uri \$uri/ $URI_TO_SCRIPT;" /etc/nginx/nginx.conf; > /dev/null
+fi
+
+if [ -n $APP_URL ]
+then
+  sed -i "/proxy_pass /c proxy_pass $URI_TO_SCRIPT;0" /etc/nginx/nginx.conf; > /dev/null
 fi
 
 exec "$@"
